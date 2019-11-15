73008783a530f4cca55420160a89a949d3f0ad0b
Preetam D'Souza <preetamjdsouza@gmail.com>
2019-11-14 22:58:50 -0500

    feat: Add plugin for Twitter Card support

---
+++ src/chloe/plugin/twitter_card.clj
@@ -0,0 +1,35 @@
+(ns chloe.plugin.twitter-card
+  (:require [clojure.string :as str]
+            [net.cgrand.enlive-html :as html]))
+
+(defn html?
+  "Check if a path contains HTML content."
+  [path]
+  (or (str/ends-with? path "/")
+      (str/ends-with? path ".html")))
+
+(defn twitter-meta-tag [property content]
+  (when content
+    (html/html [:meta {:name property :content content}])))
+
+(defn inject-tags
+  "Inject Twitter Card tags into a page."
+  [page base-url]
+  (html/deftemplate template (java.io.StringReader. (page :content))
+    []
+    [:head] (html/append (twitter-meta-tag "twitter:title" (page :title)))
+    [:head] (html/append (twitter-meta-tag "twitter:description" (page :description)))
+    ; twitter:image requires an absolute path
+    [:head] (html/append (twitter-meta-tag "twitter:image" (when-let [img (page :image)]
+                                                             (str base-url img))))
+    [:head] (html/append (twitter-meta-tag "twitter:card" "summary")))
+  (assoc page :content (apply str (template))))
+
+(defn twitter-card
+  "Add Twitter Card support to pages."
+  [site]
+  (update site :partials #(map (fn [page]
+                                (if (html? (page :url))
+                                  (inject-tags page (site :url))
+                                  page))
+                              %)))
